42310cf647f818b965027a98d60758c0562fa4f8
wmaliga <wojciech.maliga@gmail.com>
2022-07-25 12:44:38 +0200

    Minimal application code

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders main app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Note Share React/i);
   expect(linkElement).toBeInTheDocument();
 });
