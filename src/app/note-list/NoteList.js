0ffa7a414147c8de0a9bcf150d264e479c090d54
wmaliga <wojciech.maliga@gmail.com>
2022-07-27 09:16:41 +0200

    Note list component implementation

--- src/app/note-list/NoteList.js
+++ src/app/note-list/NoteList.js
@@ -1,35 +1,60 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
 
 import './NoteList.css';
 
 function NoteList() {
+  const [notes, setNotes] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/notes', {method: 'GET'})
+      .then(response => {
+        if (response.status !== 200) {
+          throw new Error('Ups! Something gone wrong...')
+        }
+        return response.json();
+      }).then(setNotes)
+      .catch(setError);
+  }, []);
+
+  let errorElement = <div/>;
+  let spinnerElement = <div/>;
+
+  const sections = notes.map(note => {
+    const expirationDate = new Date(note.expirationDate).toISOString().slice(0, 10);
+    const tagColor = note.type === 'PRIVATE' ? 'tag-red' : 'tag-green'
+
+    return (
+      <section key={note.id} className="note-container">
+        <h2 className="note-title"><a href={`/note/${note.id}`}>{note.title}</a></h2>
+        <p className="note-meta">Note #{note.id}
+          <span> expires after: {expirationDate} </span>
+          <span className={`tag ${tagColor}`}>{note.type.toLowerCase()}</span>
+        </p>
+      </section>
+    );
+  });
+
+  if (error) {
+    errorElement = (
+      <div className="error-container">{error.message}</div>
+    );
+  }
+
+  if (!notes && !error) {
+    spinnerElement = (
+      <div className="align-center">
+        <div id="loading"></div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1 className="content-subhead">Public Notes</h1>
-
-      <section className="note-container">
-        <h2 className="note-title">Test note 1</h2>
-        <p className="note-meta">Note #xxx-xxx-xxx
-          <span> expires after: 2022-12-12 </span>
-          <span className="tag tag-green">Public</span>
-        </p>
-      </section>
-
-      <section className="note-container">
-        <h2 className="note-title">Test note 2</h2>
-        <p className="note-meta">Note #xxx-xxx-xxx
-          <span> expires after: 2022-12-12 </span>
-          <span className="tag tag-red">Private</span>
-        </p>
-      </section>
-
-      <section className="note-container">
-        <h2 className="note-title">Test note 3</h2>
-        <p className="note-meta">Note #xxx-xxx-xxx
-          <span> expires after: 2022-12-12 </span>
-          <span className="tag tag-green">Public</span>
-        </p>
-      </section>
+      {sections}
+      {errorElement}
+      {spinnerElement}
     </div>
   );
 }
--- src/app/note-list/NoteList.test.js
+++ src/app/note-list/NoteList.test.js
@@ -1,9 +1,69 @@
+// noinspection JSCheckFunctionSignatures
+
 import {render, screen} from '@testing-library/react';
+import {act} from 'react-dom/test-utils';
 
 import NoteList from './NoteList';
 
-test('renders note list', () => {
-  render(<NoteList/>);
-  const linkElement = screen.getByText(/Public Notes/i);
-  expect(linkElement).toBeInTheDocument();
+const testNote = {
+  id: 'noteId',
+  title: 'noteTitle',
+  expirationDate: '2030-01-01',
+  type: 'PUBLIC'
+};
+
+let fetchBackup = undefined;
+
+describe('Note list', () => {
+
+  beforeEach(() => {
+    fetchBackup = fetch;
+  })
+
+  afterEach(() => {
+    fetch = fetchBackup;
+  });
+
+  test('renders title', async () => {
+    fetch = jest.fn(() => Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve([testNote])
+      })
+    );
+
+    await act(async () => await render(<NoteList/>));
+
+    const linkElement = screen.getByText(/Public Notes/);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('loads elements', async () => {
+    fetch = jest.fn(() => Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve([testNote])
+      })
+    );
+
+    await act(async () => await render(<NoteList/>));
+
+    const id = screen.getByText(testNote.id, {exact: false});
+    const title = screen.getByText(testNote.title, {exact: false});
+    const expirationDate = screen.getByText(testNote.expirationDate, {exact: false});
+    expect(id).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+    expect(expirationDate).toBeInTheDocument();
+  });
+
+  test('shows errors', async () => {
+    fetch = jest.fn(() => Promise.resolve({
+        status: 404,
+        json: () => Promise.resolve([testNote])
+      })
+    );
+
+    await act(async () => await render(<NoteList/>));
+
+    const message = screen.getByText('Ups! Something gone wrong...');
+    expect(message).toBeInTheDocument();
+  });
 });
